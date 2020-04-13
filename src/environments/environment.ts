0d7238972c8179dd607ad1d7d0576a31c81d0df7
Reuben <gathiireuben@gmail.com>
2020-04-13 16:28:01 +0300

    adds token to environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ouath_token:'73ac3fb0f8a2775e6d6e5c582d03a84e2356b363',
 };
 
 /*
