58d8c0bc7c3a7a98eee31e46a806adb392230f7d
Reuben <gathiireuben@gmail.com>
2020-04-13 16:20:01 +0300

    adds navigation events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,27 @@ export class AppComponent {
   constructor(private loadingBar: SlimLoadingBarService,
     private router: Router) {
 
-    
+    // @ts-ignore
+    this.router.events.subscribe((event: Event) => {
+      this.navigationInterceptor(event);
+    });
+  }
+
+  private navigationInterceptor(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this.loadingBar.start();
+    }
+
+    if (event instanceof NavigationEnd) {
+      this.loadingBar.complete();
+    }
+
+    if (event instanceof NavigationCancel) {
+      this.loadingBar.stop();
+    }
+
+    if (event instanceof NavigationError) {
+      this.loadingBar.stop();
+    }
   }
 }
