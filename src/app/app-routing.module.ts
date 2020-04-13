9317bba527540f2b05894dc890dd3464060795fe
Reuben <gathiireuben@gmail.com>
2020-04-13 16:09:38 +0300

    adds routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  },
+  {
+    path: 'repositories',
+    component: RepositoriesComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
