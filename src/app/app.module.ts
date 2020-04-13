5d83cc837877c5a035a7a7b78916c4105a9f050d
Reuben <gathiireuben@gmail.com>
2020-04-13 16:25:31 +0300

    imports components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
+import { UsersComponent } from './components/users/users.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SlimLoadingBarModule} from "ng2-slim-loading-bar";
+import {ToastrModule} from "ngx-toastr";
+import { UserService } from './services/user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoriesComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    SlimLoadingBarModule.forRoot(),
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
