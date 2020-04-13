ef72a89782cf2b025959b60f42b418c768e587c6
Reuben <gathiireuben@gmail.com>
2020-04-13 17:12:16 +0300

    adds user and repository classes

---
+++ src/app/models/repository.model.ts
@@ -0,0 +1,5 @@
+export default class RepositoryModel{
+    description: string
+    full_name: string
+    html_url: string
+}
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export default class UserModel {
+    login: string
+    avatar_url: string
+    url: string
+    html_url: string
+    followers_url: string
+    repos_url: string
+    type: string
+}
